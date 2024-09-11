cc60ba67f8992b2c91c7478d580e3ec4b8efca7d
neromaxor <141302051+neromaxor@users.noreply.github.com>
2024-09-11 17:40:10 +0300

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,7 +12,9 @@ export default function Home() {
         <h1>Campers of your dreams</h1>
         <p>You can find everything you want in our catalog</p>
       </div>
-      <button onClick={handleCatalogClick}>View Now</button>
+      <button className={css.viewNow} onClick={handleCatalogClick}>
+        View Now
+      </button>
     </section>
   );
 }

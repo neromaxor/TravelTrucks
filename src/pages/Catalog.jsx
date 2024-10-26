66f0977ab412368eadc2c7190cc64e82ad30b979
neromaxor <141302051+neromaxor@users.noreply.github.com>
2024-10-27 02:04:15 +0300

    update.2.3

--- src/pages/Catalog.jsx
+++ src/pages/Catalog.jsx
@@ -1,9 +1,8 @@
 import CampersList from "../components/Campers/CampersList";
-
+import css from "./../pages/Catalog.module.css";
 export default function Catalog() {
   return (
-    <div>
-      <p>Welcome The Catalog</p>
+    <div className={css.container}>
       <CampersList />
     </div>
   );
